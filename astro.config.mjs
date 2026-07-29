8e5866c9c2e794187c5aa0f211654027ce721594
Anfitriao Airbnb <anfitrioesairbnb@gmail.com>
2026-07-29 16:19:04 -0300

    feat: adiciona robots.txt e sitemap.xml para SEO e Google Search Console

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://www.academiadosocorrista.com.br'
+  site: 'https://www.academiadosocorrista.com.br',
+  integrations: [sitemap()]
 });
